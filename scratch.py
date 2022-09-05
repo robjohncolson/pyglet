61877d090aeea80d11237e1a8c4b476949138c0f
bobby <bobby@debian-BULLSEYE-live-builder-AMD64>
2022-09-05 17:50:11 -0500

    new revisions

--- scratch.py
+++ scratch.py
@@ -1,3 +1,21 @@
-import from pyglet.window
+import pyglet
+#context = config.create_context(share)
+window = pyglet.window.Window(fullscreen=True)
+
+#window = pyglet.window.Window(fullscreen=True, visible=False, screen=screens[1])
+window.set_visible()
 
 
+
+label = pyglet.text.Label('Hello, world',
+                         font_name='Times New Roman',
+                         font_size=36,
+                         x=window.width//2, y=window.height//2,
+                         anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
