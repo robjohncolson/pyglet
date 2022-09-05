b44da0d1a0897aa6896d3f6cd7bd82124931c013
bobby <bobby@debian-BULLSEYE-live-builder-AMD64>
2022-09-05 18:12:07 -0500

    more stuff

--- scratch.py
+++ scratch.py
@@ -1,4 +1,5 @@
 import pyglet
+import pyglet.shapes
 #context = config.create_context(share)
 window = pyglet.window.Window(fullscreen=True)
 
@@ -12,6 +13,9 @@ label = pyglet.text.Label('Hello, world',
                          font_size=36,
                          x=window.width//2, y=window.height//2,
                          anchor_x='center', anchor_y='center')
+
+circle = shapes.Circle(x=100, y=150, radius=100, color=(50, 225, 30))
+square = shapes.Rectangle(x=200, y=200, width=200, height=200, color=(55, 55, 255))
 
 @window.event
 def on_draw():
